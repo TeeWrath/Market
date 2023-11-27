0784621c29f691c403b1a367a8297f8c7cf0442f
TeeWrath <subroto.2003@gmail.com>
2023-11-27 11:11:17 +0530

    feat: adding sqlite database

--- market.py
+++ market.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+# Class instance for database
+class Item(db.Model):
+    # Primary keys are those which we use to do numbering in the database
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column( db.String(length=30), nullable=False, unique=True )
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
 
 @app.route('/')
 @app.route('/home')
